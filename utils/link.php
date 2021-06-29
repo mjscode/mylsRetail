c3496b175a53b317ddf3be1f0117b6e69d776d98
Moshe Levitz <mylevitz@gmail.com>
2021-06-29 17:30:31 -0400

    fixing link

--- utils/link.php
+++ utils/link.php
@@ -3,12 +3,14 @@
 function getLink($route,$replace = []) {
     //replace is what you're looking to change.
     //add a query string to route.
-    $route.='?';
 
     //combines the current url with the replacement.
     $params = array_merge($_GET, $replace);
-
+    if(empty($params)){
+        return $route;
+    }
     // builds a new link
+    $route.='?';
     $route .= http_build_query($params);
 
     return $route;
